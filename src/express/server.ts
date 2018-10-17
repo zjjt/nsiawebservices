505404b97bb4e710f364985d5438c794bec32020
zjjt <zehijean1988@gmail.com>
2018-10-17 10:05:07 +0000

    ready for testing docker deployment

--- src/express/server.ts
+++ src/express/server.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
 import {apiRouter} from './routes';
 import makeDBconnexion from '../typeORM/connector';
+import * as cors from 'cors';
 
 export const startServer = async() => {
     const app = express();
     const port = process.env.PORT || 3000;
+    app.use(cors());
     app.use('/', apiRouter);
     await makeDBconnexion();
     const expressApp = await app.listen({
